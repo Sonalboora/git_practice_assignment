89ae3f427b59af2cafb17ce4e21e70ff6f2e9aef
ankita-barad <ankitabarad2111@gmail.com>
2023-01-07 16:54:08 +0530

    code_updated

--- prime.js
+++ prime.js
@@ -1,13 +1,12 @@
-let n=13;
-let p=0
-for(let i=1;i<n.length;i--){
-if(n%i==0){
-p++;
+let n = 13;
+let p = 0;
+for (let i = 1; i < n.length; i++) {
+  if (n % i == 0) {
+    p++;
+  }
 }
-}
-if(p==2){
-console.log("prime")
-}
-else{
-console.log("not a prime")
+if (p == 2) {
+  console.log("prime");
+} else {
+  console.log("not a prime");
 }
